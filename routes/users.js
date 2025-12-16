b4f76d6c3dd9beb63cd2be9f073e6eaf2367c88a
Imesh29 <ridrajakaruna29@gmail.com>
2025-12-16 16:27:54 +0530

    Add endpoint to get user's following list
    Introduces a new GET /:userId/following route that returns the list of followers for a user if the requester is following them or if the account is public. Returns an error if the account is private and access is not permitted.

--- routes/users.js
+++ routes/users.js
@@ -272,4 +272,24 @@ router.post("/reject-request/:requesterId", auth, async (req, res) => {
   res.json({ message: "Follow request rejected" });
 });
 
+
+router.get("/:userId/following", auth, async (req, res) => {
+  const userId = req.params.userId;
+  const currentUserId = req.user._id;
+
+  const user = await User.findById(userId);
+  if (!user) return res.status(404).json({ message: "User not found!" });
+
+  const currentUser = await User.findById(currentUserId);
+  if (!currentUser) return res.status(404).json({ message: "User not found!" });
+
+  if (currentUser.following.includes(userId) || !user.isPrivate) {
+    return res.json(user.followers);
+  } else {
+    return res
+      .status(400)
+      .json({ message: "Can't get following list - Account is private." });
+  }
+});
+
 module.exports = router;
