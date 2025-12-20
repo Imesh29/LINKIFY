c24aa9c9d1594bdd6443c8d3719e7db87430e268
Imesh29 <ridrajakaruna29@gmail.com>
2025-12-20 20:17:33 +0530

    Add group field to schema

--- models/chats.js
+++ models/chats.js
@@ -4,6 +4,11 @@ const chatSchema = new mongoose.Schema(
       {
         participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
         lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+        // for groups
+        isGroup: { type: Boolean, default: true },
+        admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        groupName: { type: String },
+        groupImage: { type: String },
       },  
       { timestamps: true }
 );
